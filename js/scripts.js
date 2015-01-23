dea33f11c5367370ce0125237a4c1aebd0817a41
jkipker <jkipker@gmail.com>
2015-01-23 12:16:07 -0600

    hamburger nav added & functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 $(function() {
+	var navOut = false;
 
+	//NAV FUNCTIONS
+	$(".nav-burger").click(function(event) {
+		if(navOut == false){
+			$(".ham-ico").hide();
+			$(".ham-close").show();	
+			$(".nav-bubble").show();
+			navOut = true;
+		} else if(navOut == true){
+			$(".ham-close").hide();	
+			$(".ham-ico").show();
+			$(".nav-bubble").hide();
+			navOut = false;
+		}
+	});
+
+
+	//GRID HOVER EFFECTS
 	$(".grid-block").hover( 
 		function() {
 			$(".block-hover", this).fadeIn();
@@ -7,5 +25,9 @@ $(function() {
 		}, function() {
 			$(".block-hover", this).fadeOut();
 	});
+
+
+
+
 
 });
